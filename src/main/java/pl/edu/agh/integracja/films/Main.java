e34e7987d05573aea13486dbb98cbf72c9a41fad
Coroneal <slkulig@student.agh.edu.pl>
2015-04-12 13:43:36 -0700

    TheMovieDbService implementation

--- src/main/java/pl/edu/agh/integracja/films/Main.java
+++ src/main/java/pl/edu/agh/integracja/films/Main.java
@@ -1,9 +1,10 @@
 package pl.edu.agh.integracja.films;
 
+import com.omertron.themoviedbapi.MovieDbException;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
-
 import pl.edu.agh.integracja.films.films.FilmsService;
 import pl.edu.agh.integracja.films.films.db.tables.pojos.Genre;
 import pl.edu.agh.integracja.films.films.db.tables.pojos.Movie;
@@ -15,29 +16,29 @@ import pl.edu.agh.integracja.films.utils.JmdbUtils;
 
 public class Main {
 
-	public static void main(String[] args) throws SQLException {
-		new Main().main();
-	}
+    public static void main(String[] args) throws SQLException, MovieDbException, ParseException {
+        new Main().main();
+    }
 
-	private JmdbService jmdbService = new JmdbService();
-	private TheMovieDbService tmdbService = new TheMovieDbService();
-	private FilmsService filmsService = new FilmsService();
+    private JmdbService jmdbService = new JmdbService();
+    private TheMovieDbService tmdbService = new TheMovieDbService();
+    private FilmsService filmsService = new FilmsService();
 
-	private Map<String, Genre> initGenres() throws SQLException {
-		filmsService.insertGenres(tmdbService.getGenres());
-		return FilmsUtils.genreMap(filmsService.getGenres());
-	}
+    private Map<String, Genre> initGenres() throws SQLException, MovieDbException {
+        filmsService.insertGenres(tmdbService.getGenres());
+        return FilmsUtils.genreMap(filmsService.getGenres());
+    }
 
-	private void main() throws SQLException {
-		Map<String, Genre> genres = initGenres();
-		tmdbService.init();
+    private void main() throws SQLException, MovieDbException, ParseException {
+        Map<String, Genre> genres = initGenres();
+        tmdbService.init();
 
-		List<Movies> movies = jmdbService.getMovies(1500);
-		for (Movies movie : movies) {
-			String title = JmdbUtils.getTitle(movie);
+        List<Movies> movies = jmdbService.getMovies(1500);
+        for (Movies movie : movies) {
+            String title = JmdbUtils.getTitle(movie);
 
-			Movie tmdbMovie = tmdbService.getMovie(title, movie.getYear());
-		}
-	}
+            Movie tmdbMovie = tmdbService.getMovie(title, movie.getYear());
+        }
+    }
 
 }
--- src/main/java/pl/edu/agh/integracja/films/themoviedb/TheMovieDbService.java
+++ src/main/java/pl/edu/agh/integracja/films/themoviedb/TheMovieDbService.java
@@ -1,10 +1,18 @@
 package pl.edu.agh.integracja.films.themoviedb;
 
+import com.omertron.themoviedbapi.MovieDbException;
+import com.omertron.themoviedbapi.TheMovieDbApi;
+import com.omertron.themoviedbapi.enumeration.SearchType;
+import com.omertron.themoviedbapi.model.movie.MovieInfo;
+import com.omertron.themoviedbapi.results.ResultList;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
+import org.yamj.api.common.exception.ApiExceptionType;
 import pl.edu.agh.integracja.films.films.FilmsService;
 import pl.edu.agh.integracja.films.films.db.tables.pojos.Genre;
 import pl.edu.agh.integracja.films.films.db.tables.pojos.Movie;
@@ -12,19 +20,58 @@ import pl.edu.agh.integracja.films.utils.FilmsUtils;
 
 public class TheMovieDbService {
 
-	private Map<String, Genre> genres;
+    private static final String API_KEY = "630b83b95c5c4b50755c42155e49dd6e";
+    private static final String language = "en";
+    private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    private TheMovieDbApi api;
 
-	public void init() throws SQLException {
-		FilmsService filmsService = new FilmsService();
-		genres = FilmsUtils.genreMap(filmsService.getGenres());
-	}
+    private Map<String, Genre> genres;
 
-	public List<Genre> getGenres() {
-		return new ArrayList<>();
-	}
+    public TheMovieDbService() {
+        try {
+            api = new TheMovieDbApi(API_KEY);
+        } catch (MovieDbException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public Movie getMovie(String title, String year) {
-		return null;
-	}
+    public void init() throws SQLException {
+        FilmsService filmsService = new FilmsService();
+        genres = FilmsUtils.genreMap(filmsService.getGenres());
+    }
+
+    public List<Genre> getGenres() throws MovieDbException {
+        List<Genre> genres = new ArrayList<>();
+        for (com.omertron.themoviedbapi.model.Genre genre : api.getGenreMovieList(language).getResults()) {
+            genres.add(new Genre(null, genre.getName()));
+        }
+        return genres;
+    }
+
+    public Movie getMovie(String title, String year) throws MovieDbException, ParseException {
+        Boolean includeAdult = true;
+        Integer page = null;
+        Integer primaryReleaseYear = null;
+        ResultList<MovieInfo> infoResultList = api.searchMovie(title, page, language, includeAdult, Integer.parseInt(year), primaryReleaseYear, SearchType.PHRASE);
+        if (infoResultList.getTotalResults() != 1) {
+            throw new MovieDbException(ApiExceptionType.MAPPING_FAILED, "empty or not unique result");
+        }
+        MovieInfo movieInfo = api.getMovieInfo(infoResultList.getResults().get(0).getId(), language);
+        Date releaseDate = new Date(format.parse(movieInfo.getReleaseDate()).getTime());
+        Double popularity = (double) movieInfo.getPopularity();
+        Double voteAverage = (double) movieInfo.getVoteAverage();
+        return new Movie(null, movieInfo.getTitle(), movieInfo.getOriginalLanguage(),
+                releaseDate, movieInfo.getBudget(), movieInfo.getRevenue(),
+                popularity, movieInfo.getVoteCount(), voteAverage, extractGenreId(movieInfo.getGenres()), null);
+
+    }
+
+    private int extractGenreId(List<com.omertron.themoviedbapi.model.Genre> genresList) {
+        if (genresList == null || genresList.isEmpty()) {
+            return 0;
+        }
+        return genres.get(genresList.get(0).getName()).getId();
+    }
 
 }
+
